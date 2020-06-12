5b84ede83de27e43bb154d3aa9f328aaf3948157
Pushkar Sabharwal <pushkar7567@gmail.com>
2020-06-13 02:36:50 +0530

    fixing backend

--- src/Components/CreateExercise.js
+++ src/Components/CreateExercise.js
@@ -1,6 +1,42 @@
 import React, { Component} from "react";
 
 class CreateExercise extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state= {
+            username: '',
+            description: '',
+            duration: 0,
+            date: new Date(),
+            users: []
+        }
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    onChangeDuration(e) {
+        this.setState({
+            duration: e.targer.value
+        });
+    }
+
+    onChangeDate(date) {
+        this.setState({
+            date: date
+        });
+    }
+
     render() {
         return (
             <div>
