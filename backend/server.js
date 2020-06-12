f2e8f453ced03ec4c6c34fbf29434af4c21f2e2f
Pushkar Sabharwal <pushkar7567@gmail.com>
2020-06-12 19:47:25 +0530

    finished backend

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,12 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
 
+const exerciseRouter = require('./routes/exercises');
+const userRouter = require('./routes/users');
+
+app.use('/exercises', exerciseRouter);
+app.use('/users', userRouter);
+
 app.listen(port, () => {
     console.log(`server is running at port: ${port}`);
 });
